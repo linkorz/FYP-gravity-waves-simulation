0067409cfb3f63b6f9f09e0c9d47b6f91d5f5243
linkorz <hengjun1996@gmail.com>
2022-03-16 15:36:05 +0800

    update

--- config_baseline.py
+++ config_baseline.py
@@ -59,6 +59,7 @@ from Earth_iso import Earth_isothermal
 (T0,rho0,P0,R,gamma,kinvisc,thermdiffus,H,C) = Earth_isothermal(Z);
  
 # If using Earth MSIS model
+#from EarthMSIS import Earth_MSIS
 #[T0,rho0,P0,R,gamma,kinvisc,thermdiffus,H,C,_,_] = Earth_MSIS(Z,10,180,2020,1,0);
 
 ## ---- Background wind ----
